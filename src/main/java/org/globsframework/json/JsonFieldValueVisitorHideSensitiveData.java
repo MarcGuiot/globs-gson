285add5c4dfe6069d29b3ba09150f75a87d9b378
Ali BELKADY <dev.devagia@gmail.com>
2022-06-28 17:02:39 +0200

    added encodeHidSensitiveData for glob array

--- src/main/java/org/globsframework/json/JsonFieldValueVisitorHideSensitiveData.java
+++ src/main/java/org/globsframework/json/JsonFieldValueVisitorHideSensitiveData.java
@@ -2,7 +2,6 @@ package org.globsframework.json;
 
 import com.google.gson.stream.JsonWriter;
 import org.globsframework.json.annottations.JsonHidValue;
-import org.globsframework.metamodel.fields.FieldValueVisitor;
 import org.globsframework.metamodel.fields.StringField;
 
 public class JsonFieldValueVisitorHideSensitiveData extends JsonFieldValueVisitor {
@@ -13,7 +12,7 @@ public class JsonFieldValueVisitorHideSensitiveData extends JsonFieldValueVisito
     @Override
     public void visitString(StringField field, String value) throws Exception {
         if (field.hasAnnotation(JsonHidValue.UNIQUE_KEY)) {
-            super.visitString(field, value != null ? "XXXX" : null);
+            super.visitString(field, value != null ? "••••" : null);
         }
         else {
             super.visitString(field, value);
